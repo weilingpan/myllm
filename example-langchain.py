886989251f909c870598388c9bdf0767b2b3e3ec
Regina <42767268+weilingpan@users.noreply.github.com>
2024-12-12 09:57:16 +0800

    langchain: Chaining together components with LangChain Expression Language (LCEL) to chain together LangChain modules

--- example-langchain.py
+++ example-langchain.py
@@ -9,7 +9,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
-os.environ["OPENAI_API_KEY"] = "xxxxx"
+os.environ["OPENAI_API_KEY"] = "xxxx"
 
 parser = StrOutputParser()
 model = ChatOpenAI(model="gpt-4")
@@ -19,19 +19,28 @@ prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), 
      ("user", "{text}")]
 )
-result = prompt_template.invoke(
-    {"language": "italian",
-     "text": "hi"})
-messages = result.to_messages()
-print(f"messages={messages}")
+print(f"Prompt:\n{prompt_template}")
+
+#### 測試prompt_template ####
+# result = prompt_template.invoke(
+#     {"language": "italian",
+#      "text": "hi"})
+# messages = result.to_messages()
+# print(f"messages={messages}")
+#### 測試prompt_template ####
 
 # messages = [
 #     SystemMessage(content="You are a helpful assistant"),
 #     HumanMessage(content="hi!"),
 # ]
 
-chain = model | parser # the chain has two steps: first the language model is called, then the result of that is passed to the output parser.
-answer = chain.invoke(messages) 
+chain = prompt_template | model | parser
+answer = chain.invoke(
+    {"language": "italian", 
+     "text": "hi"}) # 加variable到prompt template
+
+# chain = model | parser # the chain has two steps: first the language model is called, then the result of that is passed to the output parser.
+# answer = chain.invoke(messages) 
 print(f"AI result: {answer}")
 
 # result = model.invoke(messages)
