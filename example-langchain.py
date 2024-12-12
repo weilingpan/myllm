003214e4903ebdfc66ce8cbc8a49ff271ff8f8cb
Regina <42767268+weilingpan@users.noreply.github.com>
2024-12-12 09:51:21 +0800

    langchain: add prompt template

--- example-langchain.py
+++ example-langchain.py
@@ -7,16 +7,28 @@ import os
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
-os.environ["OPENAI_API_KEY"] = "xxxx"
+os.environ["OPENAI_API_KEY"] = "xxxxx"
 
 parser = StrOutputParser()
 model = ChatOpenAI(model="gpt-4")
 
-messages = [
-    SystemMessage(content="You are a helpful assistant"),
-    HumanMessage(content="hi!"),
-]
+system_template = "You are a helpful assistant, Translate the following into {language}."
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), 
+     ("user", "{text}")]
+)
+result = prompt_template.invoke(
+    {"language": "italian",
+     "text": "hi"})
+messages = result.to_messages()
+print(f"messages={messages}")
+
+# messages = [
+#     SystemMessage(content="You are a helpful assistant"),
+#     HumanMessage(content="hi!"),
+# ]
 
 chain = model | parser # the chain has two steps: first the language model is called, then the result of that is passed to the output parser.
 answer = chain.invoke(messages) 
